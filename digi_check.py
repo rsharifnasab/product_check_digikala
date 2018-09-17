9d3df9103ffce862929e7f4d5be2b9f77279c515
Roozbeh <rsharifnasab@gmail.com>
2018-09-18 02:00:08 +0430

    finally everything is ok

--- digi_check.py
+++ digi_check.py
@@ -1,10 +1,8 @@
-#import this
 from re import search
 from urllib.request import urlretrieve
 
 def url_catch():
     pro_id = str (input ("please enter code of product : "))
-
     if pro_id[0] != "d" : pro_id = "dkp-" + pro_id
     pro_url = "http://www.digikala.com/product/" + pro_id
     #print("you requested this url : "pro_url)
@@ -16,10 +14,11 @@ def url_catch():
         print("product id error, pls try again")
         return url_catch()    
 
-
 print ("""dear user, welcome to this program
 this program will find price and name of a digikala product id!""");
+
 url_catch()
+
 try :
     my_file = open("tmp.html","r")
 except:
@@ -27,29 +26,23 @@ except:
     exit()
 htm = my_file.read()
 
-
-#  <meta property="og:title" content="شارژر همراه انرجایزر مدل UE15005 ظرفیت 15000 میلی آمپر ساعت">
 try :     
     name_pattern = r"""<meta property="og:title" content=".*">"""
     name_o = search(name_pattern,htm)
     name = htm[name_o.start()+35:name_o.end()-2]
 except :
-    print ("im really sorry , i cant find product name , pls try again later")
+    print ("***im really sorry , i cant find product name , pls try again later***")
 
-#c-price__value ۱۹۹,۰۰۰ <span
-#       span class="js-price-value"> ۱۹۹,۰۰۰ </span>
-# "price": 1990000,
 try:
-    price_pattern=r"""price.:.{50},"""
+    price_pattern=r"""meta name="twitter:data1" content=".*">"""
     price_o = search(price_pattern,htm)
-    price = htm[price_o.start()+1:price_o.end()-1]
+    price = htm[price_o.start()+35:price_o.end()-2]
 except :
-    print("im really sorry , i cant find product price , pls try again later")
+    print("***im really sorry , i cant find product price , pls try again later***")
 
 try:
-    print(name)
-    print(price)
+    print(" your product name is : ",name," \n and price is :",price)
 except :
-    pass
+    print("print error")
 
-#        252167
+print("sorry for persian text problem!")
