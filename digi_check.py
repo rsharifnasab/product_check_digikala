da97b5919e35779d729c9f8f6661e0b9fbf280e4
Roozbeh <rsharifnasab@gmail.com>
2018-09-18 00:18:05 +0430

    git ignore for html added and input system is good now!

--- digi_check.py
+++ digi_check.py
@@ -1,5 +1,13 @@
 #import this
-import urllib
+from urllib.request import urlretrieve
+print ("""dear user, welcome to this program
+this program will find price and name of a digikala product id!""")
 
-urllib.request.urlretrieve("digikala.com","tmp.html")
-#my_file = open()
+
+pro_id = str (input ("please enter code of product : "))
+
+if pro_id[0] != "d" : pro_id = "dkp-" + pro_id
+pro_url = "http://www.digikala.com/product/" + pro_id
+#print("you requested this url : "pro_url)
+urlretrieve(pro_url,"tmp.html")
+my_file = open("tmp.html","r")
